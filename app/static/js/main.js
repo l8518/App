e20a8f36d1d5e5867c4b02811623e610d4e58e81
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-02-21 22:19:57 +0100

    Placeholder text and removed buttons

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,12 +5,12 @@ function pull() {
         return response.json();
     })
     .then((data) => {
-        d3.select("#example")
-        .data(data)
-        .enter()
-        .append("p")
-        .attr("class", "btn btn-primary")
-        .text(function(d) { return d; });
+        // d3.select("#example")
+        // .data(data)
+        // .enter()
+        // .append("p")
+        // .attr("class", "btn btn-primary")
+        // .text(function(d) { return d; });
     });
 }
 
