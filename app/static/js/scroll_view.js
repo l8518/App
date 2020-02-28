a70514d318b0a787c3fd20c74cb4828720414345
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-02-28 10:37:10 +0100

    Quick input types added

--- app/static/js/scroll_view.js
+++ app/static/js/scroll_view.js
@@ -1,7 +1,78 @@
-get_images = function(index){
+// Age selection
+const ageSlider = document.getElementById("age_slider");
+const ageRangeText = document.getElementById("ageRange");
+ageRangeText.value = "0 - 100"; // place holder value
+
+function ageSliderChange(){
+	ageRangeText.value = age_slider.value.replace(",", " - ");
+}
+
+const maleFilterBtn = document.getElementById("maleFilter");
+var isMaleSelected = false;
+// Male female filtering
+function maleClick(){
+	if(isMaleSelected){
+		var selectedBtn = document.createAttribute("class");
+		selectedBtn.value = "btn btn-outline-secondary";
+		maleFilterBtn.setAttributeNode(selectedBtn)
+		isMaleSelected = false
+	} else{
+		var selectedBtn = document.createAttribute("class");
+		selectedBtn.value = "btn btn-secondary";
+		maleFilterBtn.setAttributeNode(selectedBtn)	
+		isMaleSelected = true
+	}
+}
+
+const femaleFilterBtn = document.getElementById("femaleFilter");
+var isFemaleSelected = false;
+function femaleClick(){
+	if(isFemaleSelected){
+		var selectedBtn = document.createAttribute("class");
+		selectedBtn.value = "btn btn-outline-secondary";
+		femaleFilterBtn.setAttributeNode(selectedBtn)
+		isFemaleSelected = false
+	} else{
+		var selectedBtn = document.createAttribute("class");
+		selectedBtn.value = "btn btn-secondary";
+		femaleFilterBtn.setAttributeNode(selectedBtn)	
+		isFemaleSelected = true
+	}
+}
+
+// Dropdown
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownClick() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+} 
+
+// Continous scroll
+get_images = function(index, schouldClear){
 	fetch('/api/images?index='+index).then(function(resp) {
 		return resp.json()
 	}).then(function (data){
+		if (schouldClear) {
+			while (mainView.childElementCount > 0){
+				mainView.removeChild(mainView.children[0]) // todo not the buttons
+			}
+		};
 		for (var i = 0; i < data.length; i++) {
 			var img = document.createElement("img");
 			var container = document.createElement("div");
