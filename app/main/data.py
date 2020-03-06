177c559eabd33930ccfdfda0efdd9c70189d689d
luusluus <lucienmartijn@gmail.com>
2020-03-06 19:20:23 +0100

    added slider plus image load

--- app/main/data.py
+++ app/main/data.py
@@ -7,12 +7,19 @@ from . import models
 # stats_ams_meta = pd.read_csv("app/data/ams_stats_infovis_metadata.csv", sep=";")
 portraits_meta = pd.read_csv("omniart_eye_dataset/omniart_metadata.csv")
 heatmap_csv = pd.read_csv("data/heatmap.csv")
-##########################
+test_data = pd.read_csv("data/sp500.csv")
+portrait_data = pd.read_csv("data/portraits.csv")
 
-# TODO: We can do the data stuff here
+##########################
 
 def get_portraits_by_year(begin_date:str, end_date:str):
     return portraits_meta.query(begin_date + ' <= creation_year <= ' + end_date)
 
 def get_heatmap():
     return heatmap_csv
+
+def get_portraits():
+    return portrait_data
+
+def get_testdata():
+    return test_data
--- app/main/routes.py
+++ app/main/routes.py
@@ -8,10 +8,6 @@ from . import data
 def index():
 	return render_template("home.html")
 
-@main.route('/temp', methods=['GET'])
-def temp():
-	return render_template("home_temp.html")
-
 @main.route('/api/portraits', methods=['GET'])
 def get_portraits_dominant_color_count():
     df = data.get_portraits_by_year('1400','1402')
@@ -35,3 +31,24 @@ def get_portraits_dominant_color_count_for_period():
 def api_helloworld():
     d = [2, 10, 10]
     return jsonify(d)
+
+@main.route('/api/portrait_count_by_year', methods=['GET'])
+def get_portrait_count_by_year():
+    portraits_df = data.get_portraits()
+    portraits_df = portraits_df.groupby(['creation_year','century']).creation_year.agg('count').to_frame('count').reset_index()    
+    return portraits_df[['creation_year','count']].to_json(orient='records')
+
+@main.route('/api/portrait_count_by_century', methods=['GET'])
+def get_portrait_count_by_century():
+    portraits_df = data.get_portraits()
+    portraits_df = portraits_df.groupby(['century']).creation_year.agg('count').to_frame('count').reset_index() 
+    return portraits_df.to_json(orient='records')
+
+@main.route('/api/morphed_image_by_year', methods=['GET'])
+def morphed_image_by_year():
+    return 0
+
+@main.route('/api/testdata', methods=['GET'])
+def testdata():
+    return data.get_testdata().to_json(orient='records')
+
