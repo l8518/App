04d23a5e5fab0d747a5031ca2fd12ea62079e8d5
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-03-11 22:42:47 +0100

    Max function for Y axis

--- app/static/js/color_distribution.js
+++ app/static/js/color_distribution.js
@@ -1,4 +1,4 @@
-var marginColorDist = {top: 50, right: 20, bottom: 10, left: 65},
+let marginColorDist = {top: 50, right: 20, bottom: 50, left: 65},
     widthColorDist = 500 - marginColorDist.left - marginColorDist.right,
     heightColorDist = 500 - marginColorDist.top - marginColorDist.bottom;
 
@@ -31,23 +31,37 @@ init_colors();
 
 const data1 = [
     {"age": "(0-2)", "0": 125, "1": 143, "2": 42, "3": 122, "4": 136, "5": 42, "6": 156, "7": 46, "8": 206},
+    {"age": "(4-6)", "0": 228, "1": 207, "2": 62, "3": 265, "4": 183, "5": 147, "6": 221, "7": 88, "8": 297},
+    {"age": "(8-12)", "0": 164, "1": 115, "2": 47, "3": 231, "4": 108, "5": 112, "6": 208, "7": 102, "8": 155},
     {"age": "(15-20)", "0": 51, "1": 35, "2": 20, "3": 106, "4": 67, "5": 57, "6": 131, "7": 35, "8": 63},
     {"age": "(25-32)", "0": 480, "1": 580, "2": 425, "3": 651, "4": 796, "5": 362, "6": 893, "7": 271, "8": 605},
     {"age": "(38-43)", "0": 41, "1": 21, "2": 18, "3": 44, "4": 40, "5": 61, "6": 52, "7": 70, "8": 33},
-    {"age": "(4-6)", "0": 228, "1": 207, "2": 62, "3": 265, "4": 183, "5": 147, "6": 221, "7": 88, "8": 297},
     {"age": "(48-53)", "0": 20, "1": 17, "2": 42, "3": 43, "4": 63, "5": 46, "6": 39, "7": 30, "8": 25},
     {"age": "(60-100)", "0": 44, "1": 44, "2": 20, "3": 40, "4": 45, "5": 19, "6": 46, "7": 14, "8": 60},
-    {"age": "(8-12)", "0": 164, "1": 115, "2": 47, "3": 231, "4": 108, "5": 112, "6": 208, "7": 102, "8": 155}
 ];
 
-function drawBars(data) {
+get_max_sum = function (data) {
+    let values = [];
+    data.forEach((ageGroup) => {
+        let sum = 0;
+        for (let i = 0; i < params['color'].length; i++) {
+            sum += ageGroup[String(params['color'][i])]
+        }
+        values.push(sum);
+    });
+    return Math.max(...values);
+};
 
-    let groups = d3.map(data1, d => d.age).keys();
+// TODO maybe sort age groups?
+function drawBars(data) {
+    let groups = d3.map(data, d => d[0]).keys();
+    console.log(groups)
     let subgroups = params['color'];
 
     // TODO max value + some extra instead of 6000
+    const maxY = Math.round((get_max_sum(data1) + (get_max_sum(data1) / 10)) / 1000) * 1000;
     let y = d3.scaleLinear()
-        .domain([0, 6000])
+        .domain([0, maxY])
         .range([heightColorDist, 0]);
 
     let x = d3.scaleBand()
@@ -87,9 +101,16 @@ function drawBars(data) {
         // enter a second time = loop subgroup per subgroup to add all rectangles
         .data(d => d)
         .enter().append("rect")
-        .attr("x", d => x(d.data.age))
-        .attr("y", d => y(d[1]))
+        .attr("x", d => {
+            console.log("X: " + d.data.key)
+            return x(d.data.age)
+        })
+        .attr("y", d => {
+            console.log("Y:" + y(d[1]))
+            return y(d[1])
+        })
         .attr("height", (d) => {
+            console.log("Y-d: " + y(d[0]) - y(d[1]))
             return y(d[0]) - y(d[1])
         })
         .attr("width", x.bandwidth())
