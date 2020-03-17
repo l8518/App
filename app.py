def000232b289127e7fca83499cebb126c73027a
Leonard Herold <git@hdld.me>
2020-03-17 01:21:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-from werkzeug.middleware.proxy_fix import ProxyFix
-from app import create_app, socketio
+from app import create_app
 
 config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
